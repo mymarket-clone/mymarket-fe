c337cfc2e0384fe3a7e9c723a692bb5097b2de46
Nika Puturidze <nikushaputuridze@gmail.com>
2025-12-07 15:02:53 +0400

    chore: make webpack modular

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,15 +1,17 @@
-export const module = {
-  rules: [
-    {
-      test: /\.scss$/,
-      use: [
-        {
-          loader: 'sass-resources-loader',
-          options: {
-            resources: ['./src/app/styles/vars.scss', './src/app/styles/mixines.scss'],
+export default {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: ['./src/app/styles/vars.scss', './src/app/styles/mixines.scss'],
+            },
           },
-        },
-      ],
-    },
-  ],
+        ],
+      },
+    ],
+  },
 }
