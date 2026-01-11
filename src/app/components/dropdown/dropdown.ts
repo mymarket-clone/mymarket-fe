34a7fe8fee7ff4646d14fae160886726f5c69fe3
Nika Puturidze <nikushaputuridze@gmail.com>
2026-01-11 22:06:47 +0400

    wip: dropdown

--- src/app/components/dropdown/dropdown.ts
+++ src/app/components/dropdown/dropdown.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core'
+import { Component, ElementRef, Renderer2, signal, viewChild } from '@angular/core'
 import { HighlightDirective } from '../../modules/directives/highlight'
 import { BaseInput } from '../../shared/components/base-input'
 import { ReactiveFormsModule } from '@angular/forms'
@@ -11,4 +11,23 @@ import { SvgIconComponent } from 'angular-svg-icon'
 })
 export class Dropdown extends BaseInput {
   public selecting = signal<boolean>(false)
+  private dropRootEl = viewChild<ElementRef<HTMLElement>>('dropRootEl')
+  private dropMenuEl = viewChild<ElementRef<HTMLElement>>('dropMenuEl')
+
+  public constructor(private readonly renderer: Renderer2) {
+    super()
+  }
+
+  public ngAfterViewInit(): void {
+    this.renderer.listen('document', 'click', (event: MouseEvent) => {
+      const rootEl = this.dropRootEl()?.nativeElement
+      const menuEl = this.dropMenuEl()?.nativeElement
+
+      const target = event.target as Node
+
+      if (rootEl?.contains(target) || menuEl?.contains(target)) return
+
+      this.selecting.set(false)
+    })
+  }
 }
