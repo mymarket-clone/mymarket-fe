e6fc0e5b8cba98ff07d187178d832ead5135d994
Nika Puturidze <nputuridze@upgaming.com>
2026-02-10 12:17:11 +0400

    feat: add missing methods to api service interface

--- src/app/interfaces/common/IApiService.ts
+++ src/app/interfaces/common/IApiService.ts
@@ -1,11 +1,14 @@
 import { User } from '../../types/User'
+import { IAddPostPayload } from '../payload/IAddPostPayload'
 import { ISendEmailVerificationPayload } from '../payload/IEmailVerificationPayload'
+import { IGetCategoriesFlatPayload } from '../payload/IGetCategoriesFlatPayload'
 import { ILoginPayload } from '../payload/ILoginPayload'
 import { IPasswordRecoveryPayload } from '../payload/IPasswordRecoveryPayload'
 import { IRegisterPayload } from '../payload/IRegisterPayload'
 import { ISendPasswordRecoveryPayload } from '../payload/ISendPasswordRecoveryPayload'
 import { IVerifyPasswordCodePayload } from '../payload/IVerifyPasswordCodePayload'
 import { ICategoryNode } from '../response/ICategoryNode'
+import { ICity } from '../response/ICity'
 import { IHttpService } from './IHttpService'
 import { IServiceRequest } from './IServiceRequest'
 
@@ -20,4 +23,9 @@ export interface IApiService {
   sendPasswordRecovery: (options: IServiceRequest<ISendPasswordRecoveryPayload, void>) => IHttpService<void>
   passwordRecovery: (options: IServiceRequest<IPasswordRecoveryPayload, void>) => IHttpService<void>
   getCategories: (options: IServiceRequest<void, ICategoryNode[]>) => IHttpService<ICategoryNode[]>
+  getCategoriesFlat: (
+    options?: IServiceRequest<IGetCategoriesFlatPayload, ICategoryNode[]>
+  ) => IHttpService<ICategoryNode[]>
+  addPost: (options?: IServiceRequest<IAddPostPayload, unknown>) => IHttpService<unknown>
+  getAllCities: (options?: IServiceRequest<void, ICity[]>) => IHttpService<ICity[]>
 }
