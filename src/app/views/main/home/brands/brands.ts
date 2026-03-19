82b67ceb97a8a1651ff0d08ba182caec148fbc6b
Nika Puturidze <nputuridze@upgaming.com>
2026-03-19 19:01:50 +0400

    wip: categories image

--- src/app/views/main/home/brands/brands.ts
+++ src/app/views/main/home/brands/brands.ts
@@ -3,19 +3,20 @@ import { Component } from '@angular/core'
 import { ApiService } from '../../../../services/http/api.service'
 import { IBrand } from '../../../../interfaces/response/IBrand'
 import { IHttpService } from '../../../../interfaces/common/IHttpService'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-brands',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './brands.html',
 })
 export class BrandComponent {
   public brandsState?: IHttpService<IBrand[]>
 
   public constructor(private readonly apiService: ApiService) {
-    this.apiService.request({
+    this.brandsState = this.apiService.request({
       method: HttpMethod.GET,
-      endpoint: `brands`,
+      endpoint: 'brands',
     })
   }
 }
