458c32d5d6cd26f6138a5d9454091bded2f750c5
Nika Puturidze <nputuridze@upgaming.com>
2026-03-25 22:29:03 +0400

    wip: sync

--- src/app/views/main/home/post-swiper/post-card/post-card.ts
+++ src/app/views/main/home/post-swiper/post-card/post-card.ts
@@ -1,16 +1,19 @@
+import { CurrencyType } from './../../../../../types/enums/CurrencyType'
 import { Component, input } from '@angular/core'
 import { Swiper } from '../../../../../components/swiper/swiper'
 import { RouterLink } from '@angular/router'
 import { IPostLite } from '../../../../../interfaces/response/IPostLite'
 import { SvgIconComponent } from 'angular-svg-icon'
+import { TranslocoDirective } from '@jsverse/transloco'
 
 @Component({
   selector: 'post-card',
   templateUrl: 'post-card.html',
-  imports: [RouterLink, SvgIconComponent],
+  imports: [RouterLink, SvgIconComponent, TranslocoDirective],
 })
 export class ProuductCard extends Swiper {
   public post = input.required<IPostLite>()
+  public currencyType = CurrencyType
 
   protected override get maxIndex(): number {
     return (this.post().images?.length ?? 1) - 1
